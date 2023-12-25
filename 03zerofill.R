ae1e90c01abc18057075590c0433c771718917dd
sprachan <sprachan@bowdoin.edu>
2023-12-25 15:57:24 -0600

    reformat dependencies, slight edit to description

--- 03zerofill.R
+++ 03zerofill.R
@@ -7,19 +7,32 @@
 #> use 6-letter banding code instead of scientific name; 
 #> convert observation counts to numbers, and make "X"'s into NAs; 
 #> convert stationary NA effort distances to 0.
+#> 
+#> It also selects only relevant columns, greatly reducing the size of the 
+#> data set. Columns selected are: checklist ID, observer ID, sampling event ID,
+#> species code, observation count, species observed, state code, latitude,
+#> longitude, protocol type, observation date, time observations started, 
+#> checklist duration, checklist distance, and number of observers.
+#> 
+#> Finally, I filter the resulting data frame to keep only stationary and
+#> traveling protocol checklists (no incidentals), checklists with fewer than 10
+#> observers, and checklists that have an actual checklist start time (not NA).
 #>
 # ------------------------------------------------------------------------------
 
-# Dependencies =================================================================
+# Load dependencies and parse options ==========================================
+## Dependencies ----
+# tools for working with eBird data
 library(auk)
+
+# data manipulation tools
 library(dplyr)
+
+# getting and using command line options for flexibility in running scripts
 library(optparse)
 
 
-# Parse Command Line Options ===================================================
-
-#> Necessary because I used Bowdoin's HPC to process these large files,
-#> because my personal computer couldn't (effectively) handle them.
+## Options ----
 
 option_list <- list(
   make_option(c("-e", "--ebdinput"), type = 'character',
@@ -50,9 +63,6 @@ cat('EBD input path: ', input_ebd, '\n',
 
 
 # Zerofilling ==================================================================
-
-
-
 species <- c('Mourning Dove',
              'American Crow',
              'Fish Crow',
@@ -61,30 +71,30 @@ species <- c('Mourning Dove',
 
 ebd_zf <- auk_zerofill(input_ebd, sampling_events = input_sed,
                        species = species, collapse = TRUE) |>
-  mutate(species_code = ebird_species(scientific_name, 'code'),
-         observation_count = na_if(observation_count, 'X'),
-         observation_count = as.numeric(observation_count),
-         effort_distance_km = case_when(protocol_type == 'Stationary' ~ 0,
-                                        .default = as.numeric(effort_distance_km))
-  ) |>
-  select(checklist_id,
-         observer_id,
-         sampling_event_identifier,
-         species_code,
-         observation_count,
-         species_observed,
-         state_code,
-         latitude,
-         longitude,
-         protocol_type,
-         observation_date,
-         time_observations_started,
-         duration_minutes,
-         effort_distance_km,
-         number_observers) |>
-  filter(protocol_type %in% c('Stationary', 'Traveling'),
-         !is.na(time_observations_started),
-         number_observers <=10)
+          mutate(species_code = ebird_species(scientific_name, 'code'),
+                 observation_count = na_if(observation_count, 'X'),
+                 observation_count = as.numeric(observation_count),
+                 effort_distance_km = case_when(protocol_type == 'Stationary' ~ 0,
+                                                .default = as.numeric(effort_distance_km))
+          ) |>
+          select(checklist_id,
+                 observer_id,
+                 sampling_event_identifier,
+                 species_code,
+                 observation_count,
+                 species_observed,
+                 state_code,
+                 latitude,
+                 longitude,
+                 protocol_type,
+                 observation_date,
+                 time_observations_started,
+                 duration_minutes,
+                 effort_distance_km,
+                 number_observers) |>
+          filter(protocol_type %in% c('Stationary', 'Traveling'),
+                 !is.na(time_observations_started),
+                 number_observers <=10)
 print('zf, filtered, and selected columns')
 # write file
 write.csv(ebd_zf, file = output_zf)
