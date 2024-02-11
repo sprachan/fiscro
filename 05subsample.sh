6e252726c14289a2dc1bd4a50198313ab2527835
sprachan <sprachan@bowdoin.edu>
2024-02-11 14:27:40 -0500

    boost sample size

--- 05subsample.sh
+++ 05subsample.sh
@@ -5,7 +5,7 @@
 # 05subsample.R takes -n (number of bins), -e (epsilon), -s (sample size), -t (plot tag).
 
 
- Rscript 05subsample.R -n 200 -e 1e-4 -s 1e6 -t 200_1M
+Rscript 05subsample.R -n 200 -e 1e-4 -s 3e6 -t 200_3M
 
 
 
