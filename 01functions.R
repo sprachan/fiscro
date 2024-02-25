de952b207f6fa8f7cb647e102098c6ace6253484
sprachan <sprachan@bowdoin.edu>
2024-02-25 18:41:42 -0500

    typo fix

--- 01functions.R
+++ 01functions.R
@@ -139,7 +139,7 @@ map_uncompared <- function(data_in, epsilon, nrow = 4, ncol = 6, year_mon = TRUE
       ggplot2::labs(fill = paste0('log(OF+', epsilon, ')'))
   }else{
     p <- dplyr::filter(data_in, day == over) |>
-         ggplot2::ggplot(ggplot::aes(x = long_bin,
+         ggplot2::ggplot(ggplot2::aes(x = long_bin,
                                      y = lat_bin,
                                      fill = log10(obs_freq+epsilon)))+
          ggplot2::geom_raster()+
