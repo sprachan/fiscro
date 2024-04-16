379793ed852e57e9c76f189a82d39d05a31a78c3
Sejal Prachand <sprachan@dhcp-204-163.student-wlan.bowdoin.edu>
2024-04-15 21:11:03 -0400

    load saved RDS instead of processing data identically/

--- m0/m0_lat.R
+++ m0/m0_lat.R
@@ -1,60 +1,30 @@
-library(terra)
 library(optparse)
-# LOAD DATA --------------------------------------------------------------------
-env_dir <- './processed_data/env_vars/prism'
-# load this as a raster template
-tmean <- terra::rast(list.files(env_dir, pattern = 'tmean', full.names = TRUE)[1])
-
-load('./processed_data/subsample.RData')
-
-
-
-subsample <- dplyr::select(subsample, 
-                           checklist_id,
-                           species_code, 
-                           species_observed, 
-                           latitude, 
-                           longitude)
-
 option_list <- list(make_option(c('-s', '--species', 
                                   action = 'store',
                                   type = 'character',
                                   help = '6 letter species code')))
+
 opt <- parse_args(OptionParser(option_list = option_list))
 
-df <- dplyr::filter(subsample, species_code == opt$s) |> dplyr::distinct()
+# can use m2 prepped data
+if(opt$s == 'fiscro'){
+  model_df <- readRDS('m2/m2_prepped_data.RDS')$fiscro
+}else if(opt$s == 'ribgul'){
+  model_df <- readRDS('m2/m2_prepped_data.RDS')$ribgul
+}else if(opt$s == 'amerob'){
+  model_df <- readRDS('m2/m2_prepped_data.RDS')$amerob
+}else{
+  stop('have not created processed data for the requested species')
+}
 
+# make this into a list
+model_obj <- list(long = model_df$x,
+                  lat = model_df$y,
+                  occ = model_df$sum,
+                  checklists = model_df$species_observed,
+                  N = length(model_df$x))
 
-# Spatial occurrence data ----------------------------
-sp::coordinates(df) <- ~longitude+latitude
-
-df <- terra::vect(df)
-terra::crs(df) <- terra::crs(tmean)
-
-list_layer <- terra::rasterize(df, tmean,
-                               field = 'species_observed',
-                               fun = length)
-names(list_layer) <- 'num_lists'
-
-obs_layer <- terra::rasterize(df, tmean,
-                              field = 'species_observed',
-                              fun = sum,
-                              na.rm = TRUE)
-names(obs_layer) <- 'occ'
-
-mean_layer <- obs_layer/list_layer
-names(mean_layer) <- 'obs_freq'
-
-rast_data <- c(list_layer, obs_layer) |> c(mean_layer) 
-
-# make STAN compatible ---------------------------------------------------------
-stan_df <- terra::as.data.frame(rast_data, xy = TRUE) |> na.omit()
-
-model_obj <- list(long = stan_df$x,
-                  lat = stan_df$y,
-                  occ = stan_df$occ,
-                  checklists = stan_df$num_lists,
-                  N = length(stan_df$x))
+# fit model
 model <- rstan::stan_model('m0/m0_lat.stan')
 fit = rstan::sampling(model, 
                       model_obj, 
@@ -63,4 +33,4 @@ fit = rstan::sampling(model,
                       cores = 3,
                       verbose = TRUE)
 str(fit)
-saveRDS(fit, file = file.path('~', 'eBird_project', 'model_outputs', paste0(opt$s, '_m0_lat_fit.RDS')))
+saveRDS(fit, file = file.path('~', 'eBird_project', 'model_outputs', paste0(opt$s, '_m0lat_fit.RDS')))
