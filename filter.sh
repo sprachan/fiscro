c108b429f414b6213b8dd81097d7149f96017313
Sejal Prachand <sprachan@Sejals-MacBook-Pro.local>
2024-08-26 15:30:06 -0500

    filter shell script created

---
+++ filter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -c 16
+
+for i in us ca;
+  do Rscript 02filtering.R --ebdinput ./data/${n}_amecro_ebd.txt --sedinput ./data/${n}_amecro_sampling.txt --ebdoutput ./filtered_data/${n}_amecro_ebd.txt --sedoutput ./filtered_data/${n}_amecro_sampling.txt;
+done
