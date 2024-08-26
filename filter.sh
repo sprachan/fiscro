50bab7ec5bde6e1adef9dfb21c7e5deafecf246a
Sejal Prachand <sprachan@Sejals-MacBook-Pro.local>
2024-08-26 15:41:07 -0500

    adjust loop

--- filter.sh
+++ filter.sh
@@ -2,6 +2,6 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH -c 16
 
-for i in us ca;
+for n in us ca;
   do Rscript 02filtering.R --ebdinput ./data/${n}_amecro_ebd.txt --sedinput ./data/${n}_amecro_sampling.txt --ebdoutput ./filtered_data/${n}_amecro_ebd.txt --sedoutput ./filtered_data/${n}_amecro_sampling.txt;
 done
