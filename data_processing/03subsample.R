e82a49fdf1500735dd0e468e698885f98d066af8
Sejal Prachand <sprachan@Sejals-MacBook-Pro.local>
2024-09-09 15:41:01 -0500

    Change subsampling regime and work with arrow
    - Complete overhaul of subsampling regime. Now subsamples
    without using a kernel density estimator, which did not work
    on large data.
    
    - Use arrow to work with parquet files without reading large
    data into the memory or R environment. This should make the
    subsampling much more computationally efficient.
    
    - Changes to the functions file remove functions that were
    required by the previous subsampling regime but are now
    obsolete.

--- data_processing/03subsample.R
+++ data_processing/03subsample.R
@@ -1,31 +1,26 @@
 # DESCRIPTION ------------------------------------------------------------------
 #>
-#> This script uses a kernel density estimator to estimate checklist density
-#> over a <n>x<n> spatial grid, then samples with weights inversely proportional 
-#> to the estimated density in order to reduce spatial bias in the data.
-#> The grid size, <n>, is given when this script is called in the command line.
+#> This script takes a whole, large, zero-filled eBird dataset, assigns each 
+#> checklist a discrete longitude and latitude bins (number of bins in both
+#> directions given by num_bins, passed in command line), calculates a sampled
+#> "p" as (# lists in a long-lat bin combination)/(# of all lists), then 
+#> resamples the data with weight given by 1/(p+epsilon). This produces a less
+#> spatially biased subsample with size sample_size (from command line). We 
+#> visualize the subsample, and write it to csv files partitioned by species.
 #> 
 # ------------------------------------------------------------------------------
 
-
 # Load dependencies, parse options =======================================
-## Dependencies ----
 # data manipulation tools
+library(arrow) # for working with the parquet files
 library(dplyr)
-
-# plotting tools
-library(ggplot2)
-
-# color-blind friendly color scales
-library(viridis)
-cols <- viridis(100)
-
-# kde2d function and associated help
-library(MASS)
 
 # get options from command line for flexibility
 library(optparse)
 source('functions.R')
+
+# color palettes
+library(viridis)
 ## Parse command line options ----
 option_list <- list(
   make_option(c('-n', '--numbins'), type = 'integer', 
@@ -50,7 +45,7 @@ epsilon <- opt$epsilon |> as.numeric()
 sample_size <- opt$samplesize |> as.numeric()
 tag <- opt$plottag
 
-# print out parameters used for density estimation
+# print out parameters used for rasterizing
 print(num_bins)
 print(epsilon)
 print(sample_size)
@@ -59,97 +54,75 @@ print(tag)
 # File Paths ===================================================================
 plot_path <- file.path('~', 'eBird_project', 'plots', 'subsampling')
 text_path <- file.path('~', 'eBird_project', 'subsampling_metrics')
-zf_path <- file.path('.', 'processed_data', 'combined_zf.RDS')
+pq_path <- file.path('.', 'processed_data')
 
-# Load and wrangle data ========================================================
-# print out the structure of the combined_zf data frame
-readRDS(zf_path) |> str()
+# Subsample ====================================================================
+ebd_ds <- arrow::open_dataset(file.path(pq_path, 'species'))
 
-# only need checklist id and location information (lat and long)
-lists_location <- readRDS(zf_path) |>
-                  dplyr::select(checklist_id,
-                                latitude,
-                                longitude) |>
-                  distinct(.keep_all = TRUE) # removes identical rows
-str(lists_location)
+## Process data into longitude and latitude bins, num_bins of each ----
+ext <- dplyr::select(ebd_ds, longitude, latitude) |>
+       dplyr::summarize(long_min = min(longitude, na.rm = TRUE),
+                        long_max = max(longitude, na.rm = TRUE),
+                        lat_min = min(latitude, na.rm = TRUE),
+                        lat_max = max(latitude, na.rm = TRUE)) |>
+       dplyr::collect()
 
-# Sampling =====================================================================
-# kde2d to get a probability density estimate for a given latitude and longitude
-dens <- kde2d(lists_location$longitude, 
-              lists_location$latitude,
-              n = num_bins)
-print('done kde2d')
+long_breaks <- seq(ext$long_min, ext$long_max, length.out = num_bins)
+lat_breaks <- seq(ext$lat_min, ext$lat_max, length.out = num_bins)
 
-file_name <- paste0('prob_map_', tag, '.png')
+# write this directly to a parquet file to keep things relatively faster.
+dplyr::select(ebd_ds, checklist_id, longitude, latitude) |>
+dplyr::distinct() |>
+dplyr::collect() |>
+dplyr::mutate(long_bin = findInterval(longitude, long_breaks),
+              lat_bin = findInterval(latitude, lat_breaks),
+              cell = (long_bin - 1)*num_bins + lat_bin) |>
+write_dataset(path = file.path(pq_path, 'checklists'),
+              format = 'parquet',
+              existing_data_behavior = 'overwrite')
+
+## using this binned/rasterized data, calculate sampled p adjusted by epsilon ----
+list_ds <- open_dataset(file.path(pq_path, 'checklists'))
+
+weights <- dplyr::select(list_ds, cell) |>
+           dplyr::group_by(cell) |>
+           dplyr::summarize(num_lists = dplyr::n()) |>
+           dplyr::mutate(sampled_p = num_lists/sum(num_lists))
+
+## subsample checklists using this sampled p ----
+list_subsample <- dplyr::select(list_ds, checklist_id, cell) |>
+                  dplyr::left_join(weights, by = dplyr::join_by(cell)) |>
+                  dplyr::select(checklist_id, cell, sampled_p) |>
+                  dplyr::collect() |>
+                  dplyr::slice_sample(n = sample_size,
+                                      weight_by = 1/(sampled_p+epsilon))
+
+# Visualize the checklist subsample ============================================
+file_name <- paste0('subsample_map_', tag, '.png') 
+
+mat <- list_subsample |>
+       dplyr::group_by(cell) |>
+       dplyr::summarize(num_lists = dplyr::n()) |>
+       dplyr::arrange(cell)
+
 png(filename = file.path(plot_path, file_name))
-  image(dens, col = cols)
-  title(main = paste(num_bins, epsilon, sample_size, sep = ', ')) 
+  image(log10(mat+1e-4),
+        col = viridis::inferno(n = 10))
 dev.off()
 
-## "flatten" the probability matrix into a vector
-#> going across each row, then down to the next.
-prob_vec <- as.vector(t(dens$z))
+# Subsample the whole data =====================================================
+# right_join keeps all the observations in y, but not x; that is, we keep the
+#> subsampled checklists, but not the other ones.
+dplyr::right_join(ebd_ds, 
+                  list_subsample, 
+                  by = dplyr::join_by(checklist_id)) |>
+dplyr::mutate(long_bin = cell %/% num_bins + 1,
+              lat_bin = cell %% num_bins,
+              lat_bin = dplyr::case_when(lat_bin != 0 ~ lat_bin,
+                                         .default = num_bins)) |>
+write_csv_dataset(path = file.path(pq_path, 'subsamples'),
+                  partitioning = c('species_code'),
+                  existing_data_behavior = 'overwrite')
 
-# Need to assign each checklist a spatial "bin" that matches a "bin"
-#> in the kde2d lat/long (stored in dens$y and dens$x, respectively)
+remove(list_subsample) # save some memory
 
-lists_location <- mutate(lists_location,
-                         long_bin = unlist(lapply(lists_location$longitude,
-                                                  FUN = get_bin,
-                                                  dens$x)),
-                         lat_bin = unlist(lapply(lists_location$latitude,
-                                                 FUN = get_bin,
-                                                 dens$y)),
-            		         cell = (long_bin-1)*num_bins+lat_bin,
-            		         weight = 1/(prob_vec[cell]+epsilon))
-str(lists_location)
-
-print('bin check: ')
-print(sum(lists_location$cell == 0)) # should be 0
-
-
-# sample! do this on as small a data frame as possible to speed up run times
-list_subsample <- lists_location |> 
-                  dplyr::select(checklist_id, long_bin, lat_bin, weight) |>
-	                slice_sample(n = sample_size, 
-						                   weight_by = lists_location$weight)
-	
-print('sampled')
-rm(lists_location)
-str(list_subsample)
-saveRDS(list_subsample, file = './processed_data/list_subsample.RDS')
-
-# plot new checklist distribution
-p <- list_subsample |> group_by(long_bin, lat_bin) |>
-                       summarize(num_lists = n()) |>
-                       ggplot()+
-                       geom_point(aes(x = long_bin, 
-                                      y = lat_bin, 
-                                      color = num_lists),
-				                          size = 0.55)+
-                       scale_color_viridis()
-file_name <- paste0('subsample_map_', tag, '.png') 
-ggsave(filename = file.path(plot_path, file_name), 
-       plot = p, 
-       device = 'png', 
-       dpi = 300)
-print('distribution plotted')
-# save subsample metrics =======================================================
-prob_var <- var(prob_vec)
-file_name <- paste0('subsamp_output_', tag, '.txt')
-
-cat('Probability Variance: ', prob_var, 
-    paste(num_bins, epsilon, sample_size, sep = ', '), 
-    file = file.path(text_path, file_name))
-
-print('outputted')
-
-# use checklist sample to subset the whole data set ============================
-subsample <- readRDS(zf_path) |>
-             function(x) left_join(list_subsample,
-                                   x,
-                                   by = join_by(checklist_id))
-
-str(subsample)
-head(subsample)
-saveRDS(subsample, file = './processed_data/subsample.RDS')
